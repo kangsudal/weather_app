8bd8d30399e0fcfd7e081c795da68c8674ebf2ff
kangsudal_notebook <kangsudal@gmail.com>
2022-08-30 21:14:21 +0900

    extract widgets and example api code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_app/service/fetchData.dart';
 
-void main() async{
+void main() async {
+  fetchData();
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
@@ -28,138 +30,193 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu),
-          color: Colors.white,
-        ),
-        title: Text(
-          '동동동',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-            color: Colors.white,
-          ),
-        ],
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
+      appBar: MyAppBar(),
       extendBodyBehindAppBar: true,
       body: Column(
         children: [
           //Top
-          Expanded(
-            flex: 3,
-            child: Stack(
+          Top(),
+          //Bottom
+          Bottom(),
+        ],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget with PreferredSizeWidget{
+  const MyAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.menu),
+        color: Colors.white,
+      ),
+      title: Text(
+        '동동동',
+        style: TextStyle(color: Colors.white),
+      ),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+          color: Colors.white,
+        ),
+      ],
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class Top extends StatelessWidget {
+  const Top({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: Stack(
+        children: [
+          Positioned(
+            child: Image.asset(
+              'images/aaron-burden.jpg',
+              fit: BoxFit.cover,
+            ),
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          ),
+          SizedBox.expand(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Positioned(
-                  child: Image.asset('images/aaron-burden.jpg'),
-                  top: 0,
-                  left: 0,
-                  right: 0,
+                Text(
+                  '128°',
+                  style: TextStyle(
+                    fontSize: 80,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-                SizedBox.expand(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '128°',
-                        style: TextStyle(
-                          fontSize: 80,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        '습도, 비없음, 풍속',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+                Text(
+                  '습도, 비없음, 풍속',
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
                 ),
               ],
             ),
           ),
-          //Bottom
-          Expanded(
-            child: SizedBox.expand(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    //Today Title
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Today',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          '동, 구',
-                          style: TextStyle(fontSize: 11),
-                        ),
-                      ],
+        ],
+      ),
+    );
+  }
+}
+
+class Bottom extends StatelessWidget {
+  const Bottom({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              //Today Title
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Today',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                    //Time & Icon & celcius data Row
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        //One cell
-                        Column(
-                          children: [
-                            Text('Now'),
-                            Icon(Icons.wb_sunny_outlined),
-                            Text('-120°')
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('Now'),
-                            Icon(Icons.wb_sunny_outlined),
-                            Text('-120°')
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('Now'),
-                            Icon(Icons.wb_sunny_outlined),
-                            Text('-120°')
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('Now'),
-                            Icon(Icons.wb_sunny_outlined),
-                            Text('-120°')
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('Now'),
-                            Icon(Icons.wb_sunny_outlined),
-                            Text('-120°')
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                  ),
+                  Text(
+                    '동, 구',
+                    style: TextStyle(fontSize: 11),
+                  ),
+                ],
               ),
-            ),
+              //Time & Icon & celcius data Row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //One cell widget
+                  CellWidget(
+                    date: 'Now',
+                    icon: Icons.cloud,
+                    celcious: '-130',
+                  ),
+                  CellWidget(
+                    date: '10시',
+                    icon: Icons.cloud,
+                    celcious: '-130',
+                  ),
+                  CellWidget(
+                    date: '11시',
+                    icon: Icons.cloud,
+                    celcious: '-130',
+                  ),
+                  CellWidget(
+                    date: '12시',
+                    icon: Icons.cloud,
+                    celcious: '-130',
+                  ),
+                  CellWidget(
+                    date: '13시',
+                    icon: Icons.cloud,
+                    celcious: '-130',
+                  ),
+                ],
+              ),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CellWidget extends StatelessWidget {
+  CellWidget({
+    Key? key,
+    required this.date,
+    required this.icon,
+    required this.celcious,
+  }) : super(key: key);
+  String date;
+  IconData icon;
+  String celcious;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          FittedBox(child: Text(date),fit: BoxFit.scaleDown,),
+          FittedBox(child: Icon(icon),fit: BoxFit.scaleDown,),
+          FittedBox(child: Text('$celcious°'),fit: BoxFit.scaleDown,),
         ],
       ),
     );
